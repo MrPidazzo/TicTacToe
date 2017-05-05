4359b453906ecaf4cd19290efe9c18ef88d5f220
kiNg Pidazzo <schroeder357@gmail.com>
2017-05-04 22:45:00 -0500

    Working Game
    At this point the game will work for 1 play through. Need to add the
    ability to play again.
    Code needs to be cleaned up

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ def draw_board(board):
     print '    |    |'
     
     
-def play_game():
-    
-    print 'Welcome to TicTacToe1'
+def play_game(): 
+       
+    print 'Welcome to TicTacToe!'
     draw_board(moves)
     
     print '\n\nPlayer 1 will be X'
@@ -35,26 +35,81 @@ def play_game():
     
     while not game_won:
         take_turn(player)
-    
+    else:
+        print_winner()
+        exit()
+                           
 def take_turn(current_player):
-    global player
+    
     if current_player == 1:
         moves[get_input()-1] = PLAYER1_TOKEN
         draw_board(moves)
-        player = 2
+        is_game_won()
+
+        
     elif current_player == 2:
         moves[get_input()-1] = PLAYER2_TOKEN
         draw_board(moves)
-        player = 1
+        is_game_won()
+
+        
         
     
 def get_input():
+    
+    valid_move = False
     print "\n\nIt is player {x}'s turn".format(x=str(player))
-    player_input = raw_input('Please enter the number where you would like to move.\n')
+   
+    while not valid_move:
+        player_input = raw_input('Please enter the number where you would like to move.\n')
+        if moves[int(player_input)-1] == PLAYER1_TOKEN or moves[int(player_input)-1] == PLAYER2_TOKEN:
+            print 'That move is no longer available please try again.'
+        else:
+            valid_move = True
     return int(player_input)
     
-def is_game_won(game_won):
-    pass
+def is_game_won():
+    '''
+    Winning combos
+    1-2-3
+    4-5-6
+    7-8-9
+    7-4-1
+    8-5-2
+    9-6-3
+    1-5-9
+    7-5-3
+    '''
+    global game_won
+    global player
+    
+    if moves[0] == moves[1] and moves[0] == moves[2]:
+        game_won = True
+    elif moves[3] == moves[4] and moves[0] == moves[5]:
+        game_won = True
+    elif moves[6] == moves[7] and moves[6] == moves[8]:
+        game_won = True
+    elif moves[6] == moves[3] and moves[6] == moves[0]:
+        game_won = True
+    elif moves[7] == moves[4] and moves[7] == moves[1]:
+        game_won = True
+    elif moves[8] == moves[5] and moves[8] == moves[2]:
+        game_won = True
+    elif moves[0] == moves[4] and moves[0] == moves[8]:
+        game_won = True
+    elif moves[6] == moves[4] and moves[6] == moves[2]:
+        game_won = True
+    else:
+        if player == 1:
+            player = 2
+        elif player == 2:
+            player = 1
+        
+def print_winner():
+    
+    print '\n\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+    print 'Player {x} is the winner!!!'.format(x=str(player))
+    print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     
     
 
